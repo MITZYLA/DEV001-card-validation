1e336b2cde78ec5e22b6f2e7325c026a31d4e4d3
mitzyta <andreammasoterapeuta@gmail.com>
2022-09-27 22:45:19 -0300

    agregado input tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+// const btnYes = document.getElementById("btn-debito");
+// const btnYes = document.getElementById("btn-tarjeta de credito"); 
+// const btnYes = document.getElementById("btn-transferencia");
+const btnPay = document.getElementById("btn-pay");
+const stnPay = document.getElementById("stn-pay")
+
+
+  stnPay.style.display =
+    "none";
+btnPay.addEventListener('click', () => {    
+    stnPay.style.display =
+    "block";   
+    });
