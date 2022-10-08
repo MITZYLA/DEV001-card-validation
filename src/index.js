40b3c73fd5931daacb4785af1bd92643f0a33bd2
mitzyta <andreammasoterapeuta@gmail.com>
2022-10-07 23:07:24 -0300

    cardvalidation

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import validator from './validator.js';
 console.log(validator);
 
 
-// const btnYes = document.getElementById("btn-debito");
-// const btnYes = document.getElementById("btn-tarjeta de credito"); 
-// const btnYes = document.getElementById("btn-transferencia");
+//estos son los botones de paqgo de cada tratamiento;
 const btnPay = document.getElementById("btn-pay");
-const stnPay = document.getElementById("stn-pay")
+const btnPay2 = document.getElementById("btn-pay2")
+const btnPay3 = document.getElementById("btn-pay3")
 
+//aqui use question2 y card2 para llamar al id desde html;
+const stnPay = document.getElementById("stn-pay");
+const question = document.getElementById("question");
+const question2 = document.getElementById("question2");
+const card = document.getElementById('card')
+const card2 = document.getElementById('card2')
 
-  stnPay.style.display =
-    "none";
-btnPay.addEventListener('click', () => {    
-    stnPay.style.display =
-    "block";   
-    });
+//para que aparezcan objetos o funciones ocultas;
+btnPay.addEventListener('click', () => {stnPay.style.display ="block"; });
+btnPay2.addEventListener('click', () => {stnPay.style.display ="block"; });
+btnPay3.addEventListener('click', () => {stnPay.style.display ="block"; });
+
+// quí se especificó cada boton de question, 1y2;
+question.addEventListener('click', () => {card.style.display='block';});
+question2.addEventListener('click', () => {card2.style.display='block';});
